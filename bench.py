997bc2eb9dad9604b79bd1efd7a76c4683bbd6dc
Phil Sun <philwsun@gmail.com>
2016-07-25 21:10:21 -0700

    Added perf test

---
+++ bench.py
@@ -0,0 +1,18 @@
+import veb, cProfile
+
+def run():
+	a = veb.VEB(4)
+	lim = 1 << (1 << 4)
+	for i in xrange(lim):
+		a.insert(i)
+	for i in xrange(lim):
+		nxt = a.next(i-1)
+		assert(nxt == i)
+	for i in xrange(lim):
+		a.delete(i)
+	assert(a.isEmpty())
+	for i in xrange(lim):
+		assert(a.next(i) is None)
+
+if __name__ == "__main__":
+	cProfile.run("run()")

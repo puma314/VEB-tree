4a755320b9a08277dffc9d11217474ccdb9a1deb
Uma <uma.roy.us@gmail.com>
2016-07-25 20:40:31 -0700

    Changed bsize to bitshifts

--- veb.py
+++ veb.py
@@ -7,10 +7,9 @@ class VEB:
 		self.max = -1
 
 		if self.usize != 2:
-			# block size is square root of universe size
-			self.bsize = 1 << (1 << (power - 1))
-			self.aux = VEB(power-1)
-			self.children = [VEB(power-1) for i in xrange(self.bsize)]
+			self.numbits = 1 << (power-1)
+			self.aux = VEB(power-1) 
+			self.children = [VEB(power-1) for i in xrange(1 << self.numbits)]
 		#else:
 		#	self.a = 0
 		#	self.b = 0
@@ -22,9 +21,9 @@ class VEB:
 		if self.usize == 2:
 			res = (key != self.min and key != self.max)
 		else:
-			block = key // self.bsize
+			block = key >> self.numbits
 			self.aux.insert(block)
-			res = self.children[block].insert(key % self.bsize)
+			res = self.children[block].insert(key & ((1 << self.numbits) - 1))
 
 		self.min = min(self.min, key) 
 		self.max = max(self.max, key)
@@ -38,12 +37,13 @@ class VEB:
 			return self.min
 		if key >= self.max:
 			return None
-		block = key//self.bsize
-		pos = key % self.bsize
+
+		block = key >> self.numbits
+		pos = key & ((1<< self.numbits) - 1)
 		if pos < self.children[block].max:
 			return key - pos + self.children[block].next(pos)
 		nextBlock = self.aux.next(block)
-		return key - pos + self.children[nextBlock].min + (nextBlock - block) * self.bsize
+		return key - pos + self.children[nextBlock].min + ((nextBlock - block)  << self.numbits)
 
 	def delete(self, key):
 		if self.isEmpty(): 
@@ -59,8 +59,8 @@ class VEB:
 				self.max = self.min
 			return
 		
-		block = key//self.bsize
-		self.children[block].delete(key % self.bsize)
+		block = key >> self.numbits
+		self.children[block].delete(key & ((1<<self.numbits) - 1))
 
 		if self.children[block].isEmpty(): 
 			self.aux.delete(block)
@@ -69,9 +69,9 @@ class VEB:
 			self.min = self.usize
 			self.max = -1
 		elif self.min == key:
-			self.min = self.aux.min * self.bsize + self.children[self.aux.min].min
+			self.min = (self.aux.min  << self.numbits) + self.children[self.aux.min].min
 		elif self.max == key:
-			self.max = self.aux.max * self.bsize + self.children[self.aux.max].max
+			self.max = (self.aux.max << self.numbits) + self.children[self.aux.max].max
 	
 	def __str__(self):
 		if self.usize == 2: 
@@ -87,11 +87,11 @@ a.insert(5)
 a.insert(7)
 a.insert(120)
 a.insert(88)
-print a.next(80)
-print a.next(110)
-print a.next(120)
-print a.next(3)
-a.delete(88)
-print a.next(80)
-a.delete(119)
-print a.next(110)
+print a.next(80) #88
+print a.next(110) #120
+print a.next(120) #None
+print a.next(3) #5
+a.delete(88) 
+print a.next(80) #120
+a.delete(119) 
+print a.next(110) #120

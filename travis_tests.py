72297fa75c993d3e4debca1455c6eb9fdaaa7d2a
Uma <uma.roy.us@gmail.com>
2016-07-25 22:17:48 -0700

    Modified travis tests to import from bench

--- travis_tests.py
+++ travis_tests.py
@@ -1,14 +1,5 @@
-import veb
+import bench
 
-a = veb.VEB(4)
-lim = 1 << (1 << 4)
-for i in xrange(lim):
-	a.insert(i)
-for i in xrange(lim):
-	nxt = a.next(i-1)
-	assert(nxt == i)
-for i in xrange(lim):
-	a.delete(i)
-assert(a.isEmpty())
-for i in xrange(lim):
-	assert(a.next(i) is None)
+bench.run()
+bench.sparseRun(1e4)
+
